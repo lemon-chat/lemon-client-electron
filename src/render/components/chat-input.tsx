39f889e159c40f6963e55c98af292b583a0e6195
jstzwj <1103870790@qq.com>
2021-04-02 02:51:33 +0800

    text tools

--- src/render/components/chat-input.tsx
+++ src/render/components/chat-input.tsx
@@ -2,6 +2,7 @@
 import "./chat-input.less"
 
 import React from "react";
+import { FolderOpenOutlined, ScissorOutlined } from '@ant-design/icons';
 import { Input } from 'antd';
 const { TextArea } = Input;
 
@@ -10,13 +11,18 @@ import ChatButton from "./chat-button";
 class ChatInput extends React.Component{
     render() {
         return (
-            <div className="chat-inputarea" style={{height: '100%'}}>
-                <div>
-                <FileOutlined />
+            <div className="chat-inputarea special-cursor" style={{height: '100%'}}>
+                <div className="chat-inputarea-tools">
+                    <span className="chat-inputarea-icon-wrapper">
+                        <FolderOpenOutlined className="chat-inputarea-icon" style={{fontSize: '20px'}}/>
+                    </span>
+                    <span className="chat-inputarea-icon-wrapper">
+                        <ScissorOutlined className="chat-inputarea-icon" style={{fontSize: '20px'}}/>
+                    </span>
                 </div>
                 <TextArea className="chat-inputarea-textarea" style={
                     {
-                        height: 'calc(100% - 40px)',
+                        height: 'calc(100% - 80px)',
                         resize: 'none',
                         padding: '0px 20px 0px 20px'
                     }} bordered={false} rows={4} />
